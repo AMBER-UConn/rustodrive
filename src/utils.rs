5f88989cad03d4e457f32a49b66fe11768b6b8b5
Rany Kamel <rany.kamel@uconn.edu>
2022-08-04 11:15:35 -0400

    Implemented split_data utils function

--- src/utils.rs
+++ src/utils.rs
@@ -2,9 +2,9 @@ pub fn float_to_data(fl: f32) -> [u8; 4] {
     let mut data = (fl).to_be_bytes();
     data.reverse();
     return data;
-    
 }
 
+// Combines two data entries for 4-bytes into an 8-byte entry to be sent across CAN
 pub fn combine_data(data1: [u8; 4], data2: [u8; 4]) -> [u8; 8] {
     let mut comb_data = [0; 8];
 
@@ -14,5 +14,13 @@ pub fn combine_data(data1: [u8; 4], data2: [u8; 4]) -> [u8; 8] {
     right.copy_from_slice(&data2);
 
     return comb_data;
-    
+}
+
+//Splits a data entry of 8-bytes recieved from CAN into two 4-byte entries to be read
+pub fn split_data(data: [u8; 8]) -> [[u8; 4]; 2] {
+    let err_msg = "Incorrect Data Entry Length!";
+    return  [
+            data[0..4].try_into().expect(err_msg), 
+            data[4..8].try_into().expect(err_msg),
+            ];
 }
