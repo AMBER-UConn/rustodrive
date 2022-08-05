f716af2d6799298dcc923ebcdee9f8eecc069b71
Rany Kamel <rany.kamel@uconn.edu>
2022-08-05 13:08:39 -0400

    Added several Get Commands with data returns
    Includes changes to get_heartbeat, get_temperatures, get_error (both motor & encoder), get_count, get_estimate, set_input_pos, set_input_vel, and adds get_axis_error, get_current_state, get_inverter_temperature, get_motor_temperature, get_shadow_count, get_cpr_count, get_sensorless_error, etc.

--- src/axis.rs
+++ src/axis.rs
@@ -9,7 +9,7 @@ use crate::{
         ReadComm::*,
         WriteComm::*,
     },
-    utils::*,
+    utils::ResponseManip as RData,
 };
 
 pub type AxisID = usize;
@@ -32,13 +32,31 @@ impl<'a> Axis<'a> {
         }
     }
 
-    pub fn get_heartbeat(&self) -> CANRequest {
+
+    pub fn get_heartbeat(&self) -> (u32, u8) {
+        let tk =
         ticket(
             *self.id, 
             Read(Heartbeat), 
             [0; 8],
-        )
+        );
+        
+        let (axis_error_bin, cstate_controller_bin) = RData::split_32(tk.data);
+        let (current_state_bin, controller_status_bin) = RData::split_16(cstate_controller_bin);
+
+        // Odrive CAN Signal: (0 1 2 3 4 5 6 7)
+        // Axis Error [32-bit]: (0 1 2 3)
+        // Current State [8-bit]: (4)
+        // Controller Status [8-bit]: (7)
+
+        return (
+            u32::from_le_bytes(axis_error_bin),
+            u8::from_le_bytes(RData::split_8(current_state_bin).0),
+            // TODO - Implement Controller Status Bitfield Return
+        );
     }
+    pub fn get_axis_error(&self) -> u32 {self.get_heartbeat().0}
+    pub fn get_current_state(&self) -> u8 {self.get_heartbeat().1}
 
     /// This generates the command to set the state for the `Axis` object in question
     pub fn set_state(&self, state: ODriveAxisState) -> CANRequest {
@@ -49,14 +67,24 @@ impl<'a> Axis<'a> {
         )
     }
 
-    pub fn get_temperature(&self) -> CANRequest {
+    pub fn get_temperatures(&self) -> (f32, f32) {
+        let tk =
         ticket(
             *self.id,
             Read(GetTemperature),
             [0; 8],
+        );
+
+        let (inverter_temp_bin, motor_temp_bin) = RData::split_32(tk.data);
+
+        return (
+            f32::from_le_bytes(inverter_temp_bin),
+            f32::from_le_bytes(motor_temp_bin),
         )
     }
-
+    pub fn get_inverter_temperature(&self) -> f32 {self.get_temperatures().0}
+    pub fn get_motor_temperature(&self) -> f32 {self.get_temperatures().1}
+    
     //pub fn set_control_mode
 }
 
@@ -67,36 +95,51 @@ impl<'a> Encoder<'a> {
     pub fn new(id: &'a AxisID) -> Self {
         Encoder { id }
     }
-    fn get_error(&self) -> CANRequest {
+    pub fn get_error(&self) -> u32 {
+        let tk =
         ticket(
             *self.id,
             Read(EncoderError),
             [0; 8],
-        )
+        );
+
+        u32::from_le_bytes(RData::split_32(tk.data).0)
     }
-    fn get_count(&self) -> CANRequest {
+
+    pub fn get_count(&self) -> (i32, i32) {
+        let tk =
         ticket(
             *self.id, 
             Read(GetEncoderCount), 
             [0; 8],
+        );
+        let (shadow_count_bin, cpr_count_bin) = RData::split_32(tk.data);
+
+        return (
+            i32::from_le_bytes(shadow_count_bin),
+            i32::from_le_bytes(shadow_count_bin),
         )
     }
-    fn get_estimate(&self) -> (f32, f32) {
+    pub fn get_shadow_count(&self) -> i32 {self.get_count().0}
+    pub fn get_cpr_count(&self) -> i32 {self.get_count().1}
+
+    pub fn get_estimate(&self) -> (f32, f32) {
         let tk = 
         ticket(
             *self.id,
             Read(GetEncoderEstimates),
             [0; 8],
         );
-        let (pos_est_bin, vel_est_bin) = split_data(tk.data);
-        
+        let (pos_est_bin, vel_est_bin) = RData::split_32(tk.data);
+
+
         return (
-            data_to_float(pos_est_bin),
-            data_to_float(vel_est_bin),
+            f32::from_le_bytes(pos_est_bin),
+            f32::from_le_bytes(vel_est_bin),
         )
     }
-    fn get_pos_estimate(&self) -> f32 {self.get_estimate().0}
-    fn get_vel_estimate(&self) -> f32 {self.get_estimate().1}
+    pub fn get_pos_estimate(&self) -> f32 {self.get_estimate().0}
+    pub fn get_vel_estimate(&self) -> f32 {self.get_estimate().1}
 
     fn set_linear_count() {
         unimplemented!()
@@ -124,15 +167,25 @@ impl<'a> Motor<'a> {
         Motor { id }
     }
 
-    fn get_error(&self) -> CANRequest {
+    pub fn get_error(&self) -> u64 {
+        let tk =
         ticket(
             *self.id,
             Read(MotorError),
             [0; 8],
-        )
+        );
+
+        u64::from_le_bytes(tk.data)
     }
-    fn get_sensorless_error() {
-        unimplemented!()
+    pub fn get_sensorless_error(&self) -> u32 {
+        let tk =
+        ticket(
+            *self.id,
+            Read(SensorlessError),
+            [0; 8],
+        );
+
+        u32::from_le_bytes(RData::split_32(tk.data).0)
     }
 
     fn set_node_id() {
@@ -147,7 +200,7 @@ impl<'a> Motor<'a> {
     }
 
     pub fn set_input_pos(&self, rot: f32) -> CANRequest {
-        let data = combine_data(float_to_data(rot), [0; 4]);
+        let data = RData::combine_32(rot.to_le_bytes(), [0; 4]);
         ticket(
             *self.id, 
             Write(SetInputPosition), 
@@ -155,7 +208,7 @@ impl<'a> Motor<'a> {
         )
     }
     pub fn set_input_vel(&self, speed: f32) -> CANRequest {
-        let data = combine_data(float_to_data(speed), [0; 4]);
+        let data = RData::combine_32(speed.to_le_bytes(), [0; 4]);
         ticket(
             *self.id, 
             Write(SetInputVelocity), 
